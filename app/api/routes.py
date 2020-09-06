579378115f13c9853136cc28a56241a2007d61cb
Torstein <soltors@gmail.com>
2020-09-06 05:12:31 -0400

    Revert "restructured project"
    This reverts commit 87879032c08de40966d5612edb1a180c4ab99376.

--- app/api/routes.py
+++
@@ -1,9 +0,0 @@
-from app.api import blueprint
-
-@blueprint.route('/users', methods=['GET'])
-def get_users():
-    return "test"
-
-@blueprint.route('/buffer', methods=['GET'])
-def compute_buffer():
-    return "buffer"
