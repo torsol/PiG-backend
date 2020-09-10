d092f30b49cc123d67a7bcedf722f25d46a46a53
Torstein <soltors@gmail.com>
2020-09-10 03:54:22 -0400

    made buffer work

--- app/api/operations/buffer.py
+++ app/api/operations/buffer.py
@@ -1,20 +1,46 @@
 import json
-import geopandas as gps
+import geopandas as gpd
 
-def buffer():
-    return "hello"
+EPSG_WGS84 = 'EPSG:4326'
+EPSG_UTM32V = 'EPSG:32632'
+
+
+def buffer(geoframe, value):
+    # convert to UTM32 for reference system in meters
+    data_utm32V = convert_to_UTM_32V(geoframe)
+
+    # apply buffer
+    data_utm32V_buffer = data_utm32V.buffer(value)
+
+    # convert back to WGS84
+    data_wgs84_buffer = convert_to_WGS84(data_utm32V_buffer)
+
+    return data_wgs84_buffer.to_json()
+
+def convert_request(json_request):
+    value = int(json_request['value'])
+    geodataframe = gpd.GeoDataFrame.from_features(json_request['layers']['features'])
+    geodataframe.crs = EPSG_WGS84
+
+    return geodataframe, value
+
+def convert_to_WGS84(geodataframe):
+    return geodataframe.to_crs(EPSG_WGS84)
+
+def convert_to_UTM_32V(geodataframe):
+    return geodataframe.to_crs(EPSG_UTM32V)
+
+
+
+##################################### file specific handling ###############################################
+def load_json_to_gps(fileLocation):
+    data = gpd.read_file(fileLocation)
+    data_utm32N = data.to_crs(32632)
+    return data_utm32N
 
 def load_json(fileLocation):
     data = json.load(open(fileLocation))
     return data
-
-def load_json_to_gps(fileLocation):
-    data = gps.read_file(fileLocation)
-    data_utm32N = data.to_crs(32632)
-    return data_utm32N
-
-
-
 
 if __name__ == "__main__":
     load_json("data/sample_data.json")
@@ -22,4 +48,3 @@ if __name__ == "__main__":
     data_100 = data.buffer(100)
     data_100_wgs84 = data_100.to_crs(4326)
     data_100_wgs84.to_file("countries.json", driver='GeoJSON')
-
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,15 @@
 from app.api import blueprint
+from flask import request
+
+from app.api.operations.buffer import buffer
+from app.api.operations.buffer import convert_request
 
 @blueprint.route('/users', methods=['GET'])
 def get_users():
     return "test"
 
-@blueprint.route('/buffer', methods=['GET'])
+@blueprint.route('/buffer', methods=['POST'])
 def compute_buffer():
-    return "buffer"
+    request_json = request.get_json()
+    geoframe, value = convert_request(request_json)
+    return buffer(geoframe, value)
