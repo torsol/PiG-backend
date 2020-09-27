66d5d7287e5ba472fad2dffd607e8f76e2130729
Torstein Solberg <soltors@gmail.com>
2020-09-27 18:48:49 +0200

    added some cors stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from flask_cors import CORS # disable cross-reference prevention, in order to make requests from browser
 app = Flask(__name__)
+CORS(app)
 from app.api import blueprint
 app.register_blueprint(blueprint, url_prefix='/api')
